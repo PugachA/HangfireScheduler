ece671640a913b99265d6325ebd8c1e9cb73a730
Pugach Alexander <fotone97@yandex.ru>
2020-05-14 20:47:20 +0300

    Добавлен клиент для WebScraper

---
+++ HangfireScheduler/Models/WebScraperClient.cs
@@ -0,0 +1,52 @@
+﻿using Hangfire;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HangfireScheduler.Models
+{
+    public class WebScraperClient
+    {
+        private readonly IRestClient _restClient;
+        //private readonly ILogger _logger;
+
+        public WebScraperClient(IConfiguration configuration)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException($"Значение {nameof(configuration)} не может быть null");
+
+            var webScraperUri = new Uri(configuration.GetValue<string>("WebScraperUri"));
+            _restClient = new RestClient(webScraperUri);
+        }
+
+        [AutomaticRetry(Attempts = 0)]
+        public async Task PostProductPrice(int productId)
+        {
+            var request = new RestRequest($"api/ProductWatcher/price?productId={productId}");
+
+            var response = await _restClient.ExecutePostAsync(request);
+
+            if (!response.IsSuccessful)
+            {
+                //_logger.LogError($"Не удалось получить информацию от MvcPaySystemAdmins {response.ErrorMessage} {response.ErrorException}");
+                throw new HttpRequestException($"Не удалось отправить запрос {response.ErrorMessage} {response.ErrorException}");
+            }
+        }
+
+        [AutomaticRetry(Attempts = 0)]
+        public async Task GetProductPrice(int productId)
+        {
+            var request = new RestRequest($"api/ProductWatcher/price?productId={productId}");
+
+            var response = await _restClient.ExecuteGetAsync(request);
+
+            if (!response.IsSuccessful)
+            {
+                //_logger.LogError($"Не удалось получить информацию от MvcPaySystemAdmins {response.ErrorMessage} {response.ErrorException}");
+                throw new HttpRequestException($"Не удалось отправить запрос {response.ErrorMessage} {response.ErrorException}");
+            }
+        }
+    }
+}
